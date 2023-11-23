c947b52caceaa6e646862d901912e6ddfb4fa90d
Jaime Andres Salas <jsalasmoreira@uoc.edu>
2023-11-23 14:37:54 -0500

    created the silver_to_gold transformation

--- src/classes/DataCleaning.py
+++ src/classes/DataCleaning.py
@@ -87,5 +87,37 @@ class DataProcessing:
              .save(silver_path))
         return None 
 
+    @staticmethod
+    def rename_columns(df):
+        """
+        Rename the columns of the dataframe
+        """
+        columns_name = df.columns
+        for old_col_name in columns_name:
+            new_col_name = "".join(["_" + char if char.isupper() and not old_col_name[i - 1].isupper() else char for i, char in enumerate(old_col_name)]).lstrip("_")
+            #Change the name of column
+            df = df.withColumnRenamed(old_col_name, new_col_name)
+        return df
     
-            
+    def find_location_silver(self,table_name):
+        """
+        Find the location of the silver table
+        """
+        #base_path = '/mnt/data/bronze/sql_server/SalesLT/'
+        #silver_path = '/mnt/data/silver/sql_server/SalesLT/Address/'
+        base_path = self.base_path.replace("bronze","silver")
+        return base_path + table_name + "/"
+
+    def silver_to_gold(self) -> None:
+        """
+        Transform the silver tables to gold delta tables
+        """
+        for table in self._tables_name:
+            df = self.spark.read.parquet(self.find_location_silver(table_name=table))
+            df = self.rename_columns(df)
+            gold_path = self.base_path.replace("silver","gold") + table
+            (df.write
+             .format("delta")
+             .partitionBy("date_processed")
+             .save(gold_path))
+        return None
